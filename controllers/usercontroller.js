a82598a4681d421c156d4d5d6c9be3b33f83fb08
Aayush Raut <aayussraut.ar@gmail.com>
2022-06-12 20:51:51 +0545

    User Management System

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,10 +1,10 @@
 //view exports
 const res = require("express/lib/response");
 const mysql = require("mysql");
+const { connect } = require("../routes/user");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -12,74 +12,139 @@ const pool = mysql.createPool({
 });
 
 //Connect to DB
-const view = (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log("Error connecting to DB", err);
-      return;
-    } else {
-      connection.query(
-        "SELECT * FROM user WHERE user.status='active'",
-        [],
-        (err, rows) => {
-          if (err) {
-            console.log("Error querying DB", err);
-            return;
-          }
-          return res.render("index", { rows });
-        }
-      );
+const viewall = (req, res) => {
+  connection.query(
+    "SELECT * FROM user WHERE user.status='active'",
+    [],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+      }
+      return res.render("index", { rows });
     }
-  });
+  );
 };
 
+
+//finding by id
 const find = (req, res) => {
   const searchItem = req.body.search;
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log("Error connecting to DB", err);
-      return;
-    } else {
-      connection.query(
-        "SELECT * FROM user WHERE first_name=? OR last_name=? ",
-        [searchItem, searchItem],
-        (err, rows) => {
-          if (err) {
-            console.log("Error querying DB", err);
-            return;
-          }
-          return res.render("index", { rows });
-        }
-      );
+
+  connection.query(
+    "SELECT * FROM user WHERE first_name=? OR last_name=? ",
+    [searchItem, searchItem],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+      }
+      return res.render("index", { rows });
     }
-  });
+  );
 };
 
+
+// adding form
 const form = (req, res) => {
   return res.render("add-user");
 };
 
-const addUser = (req, res) => {
 
-  const {first_name,last_name,phone,email,comment}=req.body;
-  const searchItem = req.body.search;
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log("Error connecting to DB", err);
-      return;
-    } else {
+//adding new user
+const addUser = (req, res) => {
+  const {first_name,last_name,phone,email,comments}=req.body;
+  connection.query(
+    "Insert into user(first_name,last_name,phone,email,comments) values(?,?,?,?,?)",
+    [first_name, last_name, phone, email, comments],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+      }
+      return res.render("add-user", { msg: "User Added Successfully." });
+    }
+  );
+};
+
+//editing form
+const edit = (req, res) => {
+  connection.query(
+    `SELECT * FROM user WHERE id =?`,
+    [req.params.id],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+      }
+      console.log(rows);
+      return res.render("edit-user", { rows });
+    }
+  );
+};
+
+
+//updating the data
+const update = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+
+  connection.query(
+    "UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?",
+    [first_name, last_name, email, phone, comments, req.params.id],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+        
+      } 
       connection.query(
-        "Insert into user(first_name,last_name,phone,email,comments) values(?,?,?,?,?)",[first_name,last_name,phone,email,comment],
+        `SELECT * FROM user WHERE id =?`,
+        [req.params.id],
         (err, rows) => {
           if (err) {
             console.log("Error querying DB", err);
             return;
           }
-          return res.render("add-user", { msg:"User Added Successfully." });
+          console.log(rows);
+          return res.render("edit-user", { rows ,msg:`${first_name}'s record has been updated`});
         }
       );
     }
-  });
+  );
 };
 
-module.exports = { view, find ,addUser,form};
+const deleteUser=(req,res)=>{
+  // connection.query(
+  //   "DELETE FROM user WHERE user.id=?",[req.params.id],(err,result)=>{
+  //     if(err){
+  //       console.log("Error querying DB",err);
+  //       return
+  //     }
+  //     res.redirect("/")
+  //   }
+  // )
+
+  connection.query(
+    "UPDATE user SET status=? WHERE id = ?",
+    ["removed", req.params.id],
+    (err, rows) => {
+      if (err) {
+        console.log("Error querying DB", err);
+        return;
+        
+      } 
+      res.redirect("/")
+    })
+}
+
+const viewUser=(req,res)=>{
+  connection.query("SELECT * FROM user WHERE  user.id=?",[req.params.id],(err,rows)=>{
+    if(err){
+      console.log("Error querying DB".err);
+      return
+    }
+    res.render("view-user",{rows})
+  })
+}
+
+module.exports = { viewall, find, addUser, form, edit, update ,deleteUser,viewUser};
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usercontroller = require("../controllers/usercontroller");
 
-router.get("/", usercontroller.view);
+router.get("/", usercontroller.viewall);
 router.post("/", usercontroller.find);
 router.get("/adduser", usercontroller.form);
 router.post("/adduser", usercontroller.addUser);
+router.get("/editUser/:id", usercontroller.edit);
+router.post("/editUser/:id", usercontroller.update);
+router.get("/deleteUser/:id", usercontroller.deleteUser);
+router.get("/viewUser/:id", usercontroller.viewUser);
 
 module.exports = router;
